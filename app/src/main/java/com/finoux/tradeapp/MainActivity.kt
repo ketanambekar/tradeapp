0dca5643cef626c6ef6c578410b3b56f211282f4
Ketan Ambekar <krdambekar@gmail.com>
2025-05-09 09:37:04 +0530

    First version of tradeapp

---
+++ app/src/main/java/com/finoux/tradeapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.finoux.tradeapp
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.ui.Modifier
+import com.finoux.tradeapp.ui.screens.HomeScreen
+import com.finoux.tradeapp.ui.theme.TradeappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            TradeappTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(WindowInsets.systemBars.asPaddingValues())
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
