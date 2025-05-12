dd3251aaf783cfddfacd6e1cdd9f7fb018ab6d18
ketan.ambekar <ketan.ambekar@finoux.com>
2025-05-12 18:50:34 +0530

    First working prototype

--- app/src/main/java/com/finoux/tradeapp/MainActivity.kt
+++ app/src/main/java/com/finoux/tradeapp/MainActivity.kt
@@ -44,7 +44,6 @@ class MainActivity : ComponentActivity() {
                             showTradeSdk.value = false
                         }
                     }
-//                    TradeSdk.closeSdk()
                 }
             }
         }
